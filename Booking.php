aeb7c2e406c9a1b4887436df6e54d03116298d24
AyrtonLim <s3661581@student.rmit.edu.au>
2019-09-11 13:16:27 +1000

    added all images that corrrospond to the cars in the database nd select options

--- Booking.php
+++ Booking.php
@@ -4,8 +4,18 @@
 <body>
 <select id="car" style="width: 200px;" onchange="changeImage();">
     <option value="avatar">Me</option>
-    <option value="audi">Audi</option>
-    <option value="toyota">Toyota</option>
+    <option value="maruti-suzuki-alto-800">Maruti Suzuki Alto 800</option>
+    <option value="hyundai-grand-i10">Hyundai Grand i10</option>
+    <option value="maruti-suzuki-ciaz">Maruti Suzuki Ciaz</option>
+    <option value="hyundai-elantra">Hyundai Elantra</option>
+    <option value="datsun-go">Datsun Go</option>
+    <option value="maruti-suzuki-eeco">Maruti Suzuki Eeco</option>
+    <option value="mahindra-thar">Mahindra Thar</option>
+    <option value="honda-cr-v">Honda CR-V</option>
+    <option value="volvo-s60-cross-country">Volvo S60 Cross Country</option>
+    <option value="hyundai-i20-active">Hyundai i20 Active</option>
+    <option value="mercedes-amg-slc-43">Mercedes-AMG-SLC 43</option>
+    <option value="audi-a3-cabriolet">Audi A3 Cabriolet</option>
 </select>
 <img id="image" src="../../media/avatar.png" width="250" height="250" />
 <script> 
