42525fcea447af1580f37b641b7140231d296555
pajorr <pajorr2@gmail.com>
2019-09-11 17:17:50 +1000

    Added home page

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import MenuAppBar from "./menuappbar/MenuAppBar";
 import { Router, Route } from 'react-router-dom';
 import Login from "./login/userLogin";
 import Register from "./register/userRegister";
-import {mapHome} from "./map/mapHome";
+import mapHome from "./map/mapHome";
 import {history} from "../_helpers/history";
 import {GoogleApiWrapper, Map, Marker} from "google-maps-react";
 import Booking from "./booking/userBooking";
@@ -26,7 +26,7 @@ export class App extends React.Component {
               {/*</Map>*/}
               <Router history={history}>
                   <div className="App-body">
-                      <Route path="/map" component={mapHome}/>
+                      <Route exact path="/" component={mapHome}/>
                       <Route path="/login" component={Login}/>
                       <Route path="/register" component={Register}/>
                       <Route path="/booking" component={Booking}/>
